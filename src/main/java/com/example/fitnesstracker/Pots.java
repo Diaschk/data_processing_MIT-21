d64b897a4311cfda0c87acbd5cf0e83f599890dc
Diaschk <yovhymyschdiana@gmail.com>
2025-02-10 11:39:29 +0200

    Second commit

---
+++ src/main/java/com/example/fitnesstracker/Pots.java
@@ -0,0 +1,19 @@
+package com.example.fitnesstracker;
+
+public class Pots {
+    private String name;
+    private String imageUrl;
+    private String description;
+    private double price;
+    private String features;
+
+    public Pots(String name, String imageUrl, String description, double price, String features) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+        this.features = features;
+    }
+
+
+}
